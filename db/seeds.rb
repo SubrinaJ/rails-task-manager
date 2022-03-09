78a8715e3bd6be21b7bffa0bfde903370f40bed8
Subrina Joseph <subrinajoseph@hotmail.com>
2022-03-08 22:36:20 -0400

    Running app with all 7 actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating seeds...'
+
+Tasks.destroy_all
+
+puts 'Creating dishes task...'
+dishes = Task.create(title: 'Do dishes', details: 'Before lunch', completed: true)
+
+puts 'Creating wash car task...'
+wash_car = Task.create(title: 'Wash car', details: 'Later', completed: false)
+
+puts 'Creating buy groceries...'
+buy_groceries = Task.create(title: 'Buy groceries', details: 'After dinner', completed: true)
+
+puts 'Finished creating seeds'
